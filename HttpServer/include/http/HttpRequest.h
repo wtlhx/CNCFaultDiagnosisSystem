33bef79e2d6bd522107851bc32b4d0903afa132c
luohuaxu <623039714@qq.com>
2025-05-19 22:51:37 +0800

    HttpRequest类的实现

---
+++ HttpServer/include/http/HttpRequest.h
@@ -0,0 +1,62 @@
+//封装Http请求的内容
+#pragma once
+#include <string>
+#include <unordered_map>
+#include <muduo/base/Timestamp.h>
+class HttpRequest
+{
+private:
+    enum class Method
+    {
+        GET, // 请求资源
+        POST, // 提交数据
+        Valid, // 无效
+        PUT,
+        DELETE,
+        HEAD,
+        OPTIONS,
+    }; 
+
+    Method method; // 请求方法
+    std::string url; // 请求的资源
+    std::string version; // Http版本
+    std::unordered_map<std::string, std::string> pathParameters; // 路径参数
+    std::unordered_map<std::string, std::string> queryParameters; // 查询参数
+    std::unordered_map<std::string, std::string> headers; // 请求头
+    std::string contentBody; // 请求体
+    uint64_t contentLength; // 请求体长度
+
+    muduo::Timestamp receiveTime; // 接受时间
+public:
+    HttpRequest();
+    ~HttpRequest();
+    bool setMethod(const char* start, const char* end); // 设置请求方法, [start, end)是指向字符串的指针，表示请求方法的起始和结束位置
+    Method getMethod() const { return method; } // 获取请求方法 简单函数直接在头文件中实现（内联）
+
+    void setUrl(const char* start, const char* end); // 设置请求资源
+    std::string getUrl() const { return url; } // 获取请求资源
+
+    void setVersion(std::string version_) {version = version_;} // 设置Http版本
+    std::string getVersion() const { return version; } // 获取Http版本
+
+    void setPathParameters(const std::string& key, const std::string& value) {pathParameters[key] = value;} // 设置路径参数
+    std::string getPathParameters(const std::string& key) const; // 获取路径参数
+
+    void setQueryParameters(const char* start, const char* end); // 设置查询参数
+    std::string getQueryParameters(const std::string& key) const; // 获取查询参数
+
+    void addHeaders(const char* start, const char* colonPlace, const char* end); // 添加请求头
+    std::string getHeaders(const std::string& key) const; // 获取请求头
+
+    void setContentBody(const std::string& contentBody_) {contentBody = contentBody_;} // 设置请求体
+    void setContentBody(const char* start, const char* end); // 设置请求体
+    std::string getContentBody() const { return contentBody; } // 获取请求体
+
+    void setContentLength(uint64_t contentLength_) {contentLength = contentLength_;} // 设置请求体长度
+    uint64_t getContentLength() const { return contentLength; } // 获取请求体长度
+
+    void setReceiveTime(muduo::Timestamp receiveTime_) {receiveTime = receiveTime_;} // 设置接受时间
+    muduo::Timestamp getReceiveTime() const { return receiveTime; } // 获取接受时间
+};
+
+
---
+++ HttpServer/src/http/HttpRequest.cpp
@@ -0,0 +1,129 @@
+#include "include/http/HttpRequest.h"
+
+HttpRequest::HttpRequest() : method(Method::Valid), url("Unknown"),version("Unknown"), contentLength(0)
+{
+
+}
+
+HttpRequest::~HttpRequest()
+{
+}
+
+bool HttpRequest::setMethod(const char* start, const char* end)
+{
+    std::string method_(start, end);
+    if(method_ == "GET")
+    {
+        method = Method::GET;
+    }
+    else if(method_ == "POST")
+    {
+        method = Method::POST;
+    }
+    else if(method_ == "PUT")
+    {
+        method = Method::PUT;
+    }
+    else if(method_ == "DELETE")
+    {
+        method = Method::DELETE;
+    }
+    else if(method_ == "HEAD")
+    {
+        method = Method::HEAD;
+    }
+    else if(method_ == "OPTIONS")
+    {
+        method = Method::OPTIONS;
+    }
+ 
+    return method != Method::Valid;
+}
+
+void HttpRequest::setUrl(const char* start, const char* end)
+{
+    url.assign(start, end); //assign的迭代器用法，范围是[start, end)
+}
+
+std::string HttpRequest::getPathParameters(const std::string& key) const
+{
+    auto it = pathParameters.find(key);
+    if(it != pathParameters.end())
+    {
+        return it->second;
+    }
+    return "";
+}
+
+void HttpRequest::setQueryParameters(const char* start, const char* end)
+{
+    std::string querys(start, end);//查询参数部分
+    size_t andPos = 0; // &符号位置
+    size_t prePos = 0; // 前一个&符号位置
+    size_t equalPos = 0; // =符号位置
+    while((andPos = querys.find('&', prePos)) != std::string::npos)
+    {
+        equalPos = querys.find('=', prePos);
+        if(equalPos != std::string::npos && equalPos < andPos)
+        {
+            std::string key = querys.substr(prePos, equalPos - prePos);
+            std::string value = querys.substr(equalPos + 1, andPos - equalPos - 1);
+            queryParameters[key] = value;
+        }
+        prePos = andPos + 1;
+    }
+    equalPos = querys.find('=', prePos);
+    if(equalPos != std::string::npos)
+    {
+        std::string key = querys.substr(prePos, equalPos - prePos);
+        std::string value = querys.substr(equalPos + 1);
+        queryParameters[key] = value;
+    }
+}
+
+std::string HttpRequest::getQueryParameters(const std::string& key) const
+{
+    auto it = queryParameters.find(key);
+    if(it != queryParameters.end())
+    {
+        return it->second;
+    }
+    return "";
+}
+
+void HttpRequest::addHeaders(const char* start, const char* colonPlace, const char* end)
+{
+    std::string key(start, colonPlace);
+    colonPlace ++;
+    //去掉冒号后面的空格
+    while(colonPlace < end && *colonPlace == ' ')
+    {
+        colonPlace++;
+    }
+    std::string value(colonPlace, end);
+    //去掉末尾的空格
+    while(value.size() > 0 && isspace(value[value.size() - 1]))
+    {
+        value.pop_back();//value.resize(value.size() - 1);
+    }
+    headers[key] = value;
+}
+
+std::string HttpRequest::getHeaders(const std::string& key) const
+{
+    auto it = headers.find(key);
+    if(it != headers.end())
+    {
+        return it->second;
+    }
+    return "";
+}
+
+void HttpRequest::setContentBody(const char* start, const char* end)
+{
+    if(end > start)
+    {
+        contentBody.assign(start, end); //范围[start, end)
+    }
+}
+
